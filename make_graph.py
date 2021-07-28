3a8789e3cd87eee41d99673f56d3c3a3f41c1bab
Arpit <arpitfalcon1@gmail.com>
2021-07-28 18:28:31 +0530

    add: candle stick graph

--- make_graph.py
+++ make_graph.py
@@ -2,3 +2,25 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 
+
+def candleStickGraph(ohlc_data):
+    """Generate the candle stick graph for one month data
+
+    Args:
+        ohlc_data (pandas dataframe object): open-high-low-close data
+
+    Returns:
+        plotly figure object: candlestick data
+    """
+    fig = go.Figure(go.Ohlc(x=ohlc_data['time'],
+                            open=ohlc_data['open'],
+                            high=ohlc_data['high'],
+                            low=ohlc_data['low'],
+                            close=ohlc_data['close'],
+                            name='Price'))
+
+    fig.update(layout_xaxis_rangeslider_visible=False)
+    fig.update_layout(margin=dict(l=10, r=10, t=50, b=10),
+                      width=900, height=300,
+                      title='Candlestick chart for last one month')
+    return fig
