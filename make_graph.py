507e3e2777d11f20d7b204d44f6b9fa23262804c
Arpit <arpitfalcon1@gmail.com>
2021-07-28 18:40:55 +0530

    add: indicators graph

--- make_graph.py
+++ make_graph.py
@@ -1,6 +1,7 @@
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
+from feature_extraction import extractAll
 
 
 def candleStickGraph(ohlc_data):
@@ -23,4 +24,41 @@ def candleStickGraph(ohlc_data):
     fig.update_layout(margin=dict(l=10, r=10, t=50, b=10),
                       width=900, height=300,
                       title='Candlestick chart for last one month')
+    return fig
+
+
+def indicatorsGraph(prices_data):
+    """Generate the indicators graph for one month
+
+    Args:
+        prices_data (pandas dataframe object): prices data
+
+    Returns:
+        plotly figure object: candlestick data"""
+    times = prices_data['time']
+    indicators = extractAll(prices_data)
+
+    indicators['time'] = times
+
+    indicators = indicators.iloc[-(24 * 30):, :]
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=indicators['time'], y=indicators['EMA_9'], name='EMA 9'))
+    fig.add_trace(go.Scatter(
+        x=indicators['time'], y=indicators['SMA_5'], name='SMA 5'))
+    fig.add_trace(go.Scatter(
+        x=indicators['time'], y=indicators['SMA_10'], name='SMA 10'))
+    fig.add_trace(go.Scatter(
+        x=indicators['time'], y=indicators['SMA_15'], name='SMA 15'))
+    fig.add_trace(go.Scatter(
+        x=indicators['time'], y=indicators['SMA_30'], name='SMA 30'))
+    fig.add_trace(go.Scatter(
+        x=indicators['time'], y=indicators['prices'], name='prices', opacity=0.2))
+
+    fig.update(layout_xaxis_rangeslider_visible=False)
+    fig.update_layout(margin=dict(l=10, r=10, t=50, b=10),
+                      width=900, height=300,
+                      title='Price data with technical indicators for 30 days')
+
     return fig
